cb9f0a584a5ba6460a867d1e91c9029c95aaddc4
Warwick Molloy <warwick.molloy@hooroo.com>
2018-08-15 09:42:14 +1000

    Initial code commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="app" class="container">
+        <span class="spaceLeft" />
+        <h1 class="header">{{ msg }}</h1>
+        <span class="spaceLeft" />
+        <div class="welcome">
+            {{welcome}}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App!',
+      welcome: 'Wonderful!!'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+  #app {
+    color: #56b983;
+  }
+</style>
